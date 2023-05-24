d0b7977a1d15a6d0dc5f33a47530a1f269f45a0f
Tuzteno <115424176+Tuzteno@users.noreply.github.com>
2023-05-24 03:08:26 -0300

    Update stt.py

--- stt/stt.py
+++ stt/stt.py
@@ -1,62 +1,34 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
-import torch
-import io
-import librosa
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from transformers import Wav2Vec2ForCTC
 
 app = FastAPI()
 
-# Load the pre-trained model and tokenizer
-model = Wav2Vec2ForCTC.from_pretrained("jonatasgrosman/wav2vec2-large-xlsr-53-english")
-tokenizer = Wav2Vec2Tokenizer.from_pretrained("jonatasgrosman/wav2vec2-large-xlsr-53-english")
+model = Wav2Vec2ForCTC.from_pretrained("voidful/wav2vec2-xlsr-multilingual-56")
 
-# Set the device to use for the model (GPU if available, else CPU)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
+@app.post("/transcription")
+async def convert_audio_to_text(file: UploadFile):
+    """Converts an audio file to text.
 
-# Define error handlers
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request, exc):
-    return JSONResponse(
-        status_code=400,
-        content={"message": "Invalid input"}
-    )
+    Args:
+        file: The audio file to convert.
 
-@app.exception_handler(Exception)
-async def validation_exception_handler(request, exc):
-    return JSONResponse(
-        status_code=500,
-        content={"message": "Internal server error"}
-    )
+    Returns:
+        The transcript of the audio file.
 
-# Define the WebSocket endpoint for speech-to-text transcription
-@app.websocket("/transcribe")
-async def transcribe_audio(websocket: WebSocket):
-    await websocket.accept()
+    """
+    # Load the audio file.
+    try:
+        audio = await file.read()
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="File not found.")
 
-    while True:
-        try:
-            # Receive audio data from the client
-            audio_bytes = await websocket.receive_bytes()
+    # Convert the audio to text.
+    try:
+        transcript = model.predict(audio)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-            # Convert the audio data to a format that can be processed by the model
-            try:
-                audio_input, _ = librosa.load(io.BytesIO(audio_bytes), sr=16000)
-            except Exception:
-                raise ValueError("Invalid audio file format")
+    # Return the transcript.
+    return {"transcript": transcript}
 
-            # Convert the audio data to a tensor and move it to the device
-            audio_tensor = torch.tensor(audio_input).to(device)
-
-            # Perform speech-to-text transcription using the model
-            with torch.no_grad():
-                transcription = model(audio_tensor.unsqueeze(0))[0]
-                transcription = transcription.argmax(dim=-1).squeeze().tolist()
-                transcription = tokenizer.decode(transcription)
-
-            # Send the transcription to the client
-            await websocket.send_json({"transcription": transcription})
-        except WebSocketDisconnect:
-            break
