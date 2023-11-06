1a2a7a388724549cef2e0a82eee1887c8ccf887b
Tuzteno <115424176+Tuzteno@users.noreply.github.com>
2023-11-06 05:57:56 +0000

    Update tts.py
    Change to Facebook TTS

--- tts/tts.py
+++ tts/tts.py
@@ -1,64 +1,37 @@
-from fastapi import FastAPI, File
-from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
-from datasets import load_dataset
+from transformers import VitsModel, AutoTokenizer
 import torch
-import soundfile as sf
-import shutil
-import os
+import random
+import asyncio
+import websockets
 
-from tasks import run_ffmpeg
+# Function to set seed for reproducibility
+def set_seed(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
-app = FastAPI()
+# Set the seed for reproducibility
+set_seed(555)
 
-processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
-model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
-vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
+model = VitsModel.from_pretrained("facebook/mms-tts-eng")
+tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-eng")
 
-embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+# List of texts to convert to waveforms and send via WebSocket
+texts = ["Your login session has expired Please login again"]
 
-@app.post("/tts")
-async def text_to_speech(text: str):
-    inputs = processor(text=text, return_tensors="pt")
-    speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+async def send_audio_via_websocket():
+    async with websockets.connect("ws://localhost:8765") as websocket:  # Replace with your WebSocket server URL
+        for text in texts:
+            inputs = tokenizer(text, return_tensors="pt")
+            with torch.no_grad():
+                waveform = model(**inputs).waveform.cpu().numpy()[0]
 
-    speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+            # Add a debug statement to print the data type of waveform
+            print("Data type of waveform:", waveform.dtype)
 
-    audio_path = "speech.wav"
-    sf.write(audio_path, speech.numpy(), samplerate=16000)
+            await websocket.send(waveform.tobytes())
 
-    # Create HLS segments
-    segments_path = "segments"
-    os.makedirs(segments_path, exist_ok=True)
-
-    # Convert the audio file into HLS segments
-    cmd = f"ffmpeg -i {audio_path} -c:a libfdk_aac -b:a 128k -f segment -segment_time 10 {segments_path}/output%03d.aac"
-    run_ffmpeg.delay(cmd)
-
-    # Generate HLS playlist (M3U8 file)
-    playlist_path = "playlist.m3u8"
-    with open(playlist_path, "w") as playlist_file:
-        playlist_file.write("#EXTM3U\n")
-        playlist_file.write("#EXT-X-VERSION:3\n")
-        playlist_file.write("#EXT-X-TARGETDURATION:10\n")
-        playlist_file.write("#EXT-X-MEDIA-SEQUENCE:0\n")
-        segment_files = sorted(os.listdir(segments_path))
-        for i, segment_file in enumerate(segment_files):
-            playlist_file.write(f"#EXTINF:10.0,\n")
-            playlist_file.write(f"{segments_path}/{segment_file}\n")
-        playlist_file.write("#EXT-X-ENDLIST\n")
-
-    return {"url": f"http://your-server-address/{playlist_path}"}
-
-@app.get("/audio/{audio_file}")
-async def get_audio(audio_file: str):
-    return File(audio_file)
-
-@app.on_event("startup")
-async def startup_event():
-    # Clean up any previous audio files and segments
-    if os.path.exists("speech.wav"):
-        os.remove("speech.wav")
-    if os.path.exists("segments"):
-        shutil.rmtree("segments")
-    if os.path.exists("playlist.m3u8"):
-        os.remove("playlist.m3u8")
+asyncio.get_event_loop().run_until_complete(send_audio_via_websocket())
