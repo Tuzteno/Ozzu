9a0f482f89c8c2d6fa811d3f0859430065671a14
Tuzteno <115424176+Tuzteno@users.noreply.github.com>
2023-11-07 05:38:34 +0000

    Update ws-client.js

--- frontend/ws-client.js
+++ frontend/ws-client.js
@@ -1,68 +1,34 @@
-// Create a WebSocket connection to the server
-const socket = new WebSocket('ws://localhost:8765'); // Use the correct WebSocket server port
-socket.binaryType = 'arraybuffer';  // We're working with binary data
+const socket = new WebSocket('ws://localhost:8765');
+socket.binaryType = 'arraybuffer';
 
-// Audio context and queue for audio data
-let audioCtx = new AudioContext({ sampleRate: 16000 });
+const audioCtx = new AudioContext({ sampleRate: 16000 });
 let bufferQueue = [];
 let lastEndTime = 0;
 
-// Connection opened
-socket.addEventListener('open', (event) => {
-    console.log("Connected to WebSocket server");
-    
-    // Start the AudioContext when the WebSocket connection is opened (user action)
-    if (audioCtx.state === 'suspended') {
-        audioCtx.resume();
-    }
-});
+socket.addEventListener('open', () => audioCtx.state === 'suspended' && audioCtx.resume());
+socket.addEventListener('close', () => console.log("Disconnected from WebSocket server"));
+socket.addEventListener('error', (event) => console.error("WebSocket error:", event));
 
-// Connection closed
-socket.addEventListener('close', (event) => {
-    console.log("Disconnected from WebSocket server");
-});
-
-// Connection error
-socket.addEventListener('error', (event) => {
-    console.error("WebSocket error:", event);
-});
-
-// Listen for messages
 socket.addEventListener('message', (event) => {
-    // We're assuming the data is Float32Array.
-    let dataView = new Float32Array(event.data);
-    let audioData = Array.from(dataView);
+    const dataLength = event.data.byteLength;
 
-    // Queue the audio data for playback
-    queueAudio(audioData);
-});
+    if (dataLength > 0) {
+        const audioBuffer = audioCtx.createBuffer(1, dataLength / 4, audioCtx.sampleRate);
+        audioBuffer.copyToChannel(new Float32Array(event.data), 0);
 
-function queueAudio(audioData) {
-    // Create an audio buffer and source
-    let audioBuffer = audioCtx.createBuffer(1, audioData.length, audioCtx.sampleRate);
-    let source = audioCtx.createBufferSource();
-    source.buffer = audioBuffer;
+        const source = audioCtx.createBufferSource();
+        source.buffer = audioBuffer;
+        source.connect(audioCtx.destination);
 
-    // Copy audioData to buffer
-    audioBuffer.copyToChannel(Float32Array.from(audioData), 0);
+        if (audioCtx.currentTime > lastEndTime) lastEndTime = audioCtx.currentTime;
+        source.start(lastEndTime);
+        lastEndTime += audioBuffer.duration;
 
-    // Connect source to output
-    source.connect(audioCtx.destination);
+        source.onended = () => {
+            bufferQueue.shift();
+            if (bufferQueue.length === 0) lastEndTime = audioCtx.currentTime;
+        };
 
-    if (audioCtx.currentTime > lastEndTime) {
-        lastEndTime = audioCtx.currentTime;
+        bufferQueue.push(source);
     }
-
-    source.start(lastEndTime);
-    lastEndTime += audioBuffer.duration;
-
-    // When this chunk finishes playing, remove it from the queue
-    source.onended = () => {
-        bufferQueue.shift();
-        if (bufferQueue.length === 0) {
-            lastEndTime = audioCtx.currentTime;
-        }
-    };
-
-    bufferQueue.push(source);
-}
+});
